4fd370b6da99722d3448f71a09f2d3f9145bd5c0
Pedro Mendes <mendes@copasi.org>
2021-06-05 16:36:43 -0400

    fixed syntax error

--- progress_of_fit_stats.pl
+++ progress_of_fit_stats.pl
@@ -118,7 +118,7 @@ print( O1FILE "#fevals\tMean\tStd dev\tCV\tN\tMin\tMax\n");
 for( $n=0; $n < $top; $n++ )
 {
  $stdev = sqrt($m2[$n] / $count[$n]);
- $cv = $stdev / $mean[$n]
+ $cv = $stdev / $mean[$n];
  print( O1FILE "$iter[$n]\t$mean[$n]\t$stdev\t$cv\t$count[$n]\t$minval[$n]\t$maxval[$n]\n");
 }
 
